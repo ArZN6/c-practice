263c29fa0bb0a00a220ec9cdd9ede09a54101667
ArZN6 <aaronanton1976@gmail.com>
2026-01-08 15:45:23 +0530

    Add program to find unique numbers in an array
    This program reads a list of numbers and prints the unique elements.

---
+++ Unique numbers.c
@@ -0,0 +1,31 @@
+//Unique numbers
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, count;
+
+    scanf("%d", &n);
+    int arr[n];
+
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Unique elements are:\n");
+
+    for (i = 0; i < n; i++)
+    {
+        count = 0;
+
+        for (j = 0; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+                count++;
+        }
+
+        if (count == 1)
+            printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
